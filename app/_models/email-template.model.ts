7ed7d284a3b1b4684784f2db0eeea0e7318c3243
Ritik <raj769417@gmail.com>
2025-06-08 11:24:57 +0530

    fix: schema

--- app/_models/email-template.model.ts
+++ app/_models/email-template.model.ts
@@ -1,23 +1,24 @@
-import { Schema, model, Document, Types } from "mongoose";
+import { Schema, model, Document, Types, models } from "mongoose";
 
 export interface IEmailTemplate extends Document {
   title: string;
   description?: string;
-  template: string;
+  template?: string;
   user: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const emailTemplateSchema = new Schema<IEmailTemplate>(
   {
     title: { type: String, required: true },
     description: { type: String },
-    template: { type: String, required: true },
+    template: { type: String },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
   { timestamps: true }
 );
 
-export const EmailTemplate = model<IEmailTemplate>(
-  "EmailTemplate",
-  emailTemplateSchema
-);
+export const EmailTemplate =
+  models.EmailTemplate ||
+  model<IEmailTemplate>("EmailTemplate", emailTemplateSchema);

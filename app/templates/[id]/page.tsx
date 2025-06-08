355ff2b294480e5a24266acdbd92bb71b372a4fb
Ritik <raj769417@gmail.com>
2025-06-08 16:48:22 +0530

    feat: fetch template by id

--- app/templates/[id]/page.tsx
+++ app/templates/[id]/page.tsx
@@ -3,12 +3,34 @@ import NavBar from "../_components/NavBar";
 import { ItemSelectionSideBar } from "../_components/ItemSelectionSideBar";
 import { Canvas } from "../_components/Canvas";
 import { Settings } from "../_components/Settings";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { getemailTemplate } from "@/server-fns";
+import { IEmailTemplate } from "@/app/_models/email-template.model";
 
-export default function Page() {
+export default async function Page({ params }: { params: { id: string } }) {
+  const session = await auth();
+
+  const isAuthenticated = !!session?.user;
+  if (!isAuthenticated) {
+    redirect("/signin");
+  }
+
+  const { id } = await params;
+
+  const emailTemplate = (await getemailTemplate(
+    id
+  )) as unknown as IEmailTemplate;
+  if (!emailTemplate) {
+    redirect("/templates");
+  }
+
+  const templateData = JSON.parse(emailTemplate.template || "[]");
+
   return (
     <div>
-      <Provider>
-        <NavBar />
+      <Provider initialTemplateStructure={templateData}>
+        <NavBar id={id} />
 
         <div className="flex items-start justify-between h-[calc(100vh-55px)] overflow-hidden">
           <ItemSelectionSideBar />
